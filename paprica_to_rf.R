87bff65f8f39f18162a53eb0fca1dfea2805c3b5
avishekdutta14 <35264497+avishekdutta14@users.noreply.github.com>
2021-01-12 13:12:07 -0800

    Update paprica_to_rf.R

--- paprica_to_rf.R
+++ paprica_to_rf.R
@@ -9,12 +9,15 @@ write.csv(df_percent, "unique_percentage.csv")
 df2 =  read.csv ("unique_percentage.csv", header = TRUE)
 colnames(df2)[1] <- "SampleID"
 
-#Reading the metadata file
+#Reading the metadata file-the metadata file should have a column named SampleID 
+#having the sample names matching the SampleID in paprica output. Also the response/
+#dependent variables should be present in the metadata file.
 metadata =  read.csv("sample_metadata.csv", header=TRUE)
 
+#Here Sulfide is the dependent variable, you can choose independent variable of your choice
 sulfide = metadata %>%  select(SampleID, Sulfide)
 
-#merging them based on SampleID
+#merging based on SampleID
 RF_input = merge(sulfide,df2,by="SampleID")
 
 #sorting gives an even random sampling in random forest
